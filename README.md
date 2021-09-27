d9d2206567a0146cf7251cd6f8b58b4eba4bf37b
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2021-09-27 22:19:27 +0100

    created initial main view component

---
+++ README.md
@@ -0,0 +1,9 @@
+Project name: "myFlix-client"
+
+This app is a front end interface for myFlix.app
+This app is build using React.
+
+test run with Parcel:
+Result: 
+Server running at http://localhost:1234
+✨ Built in 673ms
---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+    render() {
+        resturn(
+            <div className="main-view">
+                <div>Inception</div>
+                <div>Redemption</div>
+                <div>Galdstone</div>
+            </div>
+        );
+    }
+}
