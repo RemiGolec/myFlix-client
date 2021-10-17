b32114cf465554f1e988941e04a02a3a80d82fca
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2021-10-17 01:05:47 +0100

    added registration

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -27,3 +28,11 @@ export function LoginView(props) {
     </form>
   );
 }
+
+LoginView.propTypes = {
+    login: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func.isRequired
+};
