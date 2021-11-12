d404d1241c21b956fddd69cfaa2ba9e54dd7d0c4
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2021-11-12 18:29:04 +0000

    trying to commit changes to routing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './main-view.scss';
 import axios from 'axios';
 import { Container, Row, Col, Navbar, Nav, Button } from 'react-bootstrap';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
@@ -23,10 +22,39 @@ class MainView extends React.Component {
     };
   }
 
-  /* NOT SURE THIS CODE IS CORRECT */
-  onRegistration(user) {
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      })
+      this.getMovies(accessToken);
+    }
+  }
+
+  getMovies(token) {
+    axios.get('https://morning-badlands-52426.herokuapp.com/movies', {
+      headers: { Authorization: 'Bearer ${token}' }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+  /*When a movie is clicked, 
+  sthis function is invoked and 
+  updates the state of the `selectedMovie` *property to that movie*/
+
+  setSelectedMovie(movie) {
     this.setState({
-      user
+      selectedMovie: movie
     });
   }
 
@@ -45,85 +73,73 @@ class MainView extends React.Component {
   }
 
 
-
-
-  getMovies(token) {
-    axios.get('https://morning-badlands-52426.herokuapp.com/movies', {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  componentDidMount() {
-    let accessToken = localStorage.getItem('token');
-    if (accessToken !== null) {
-      this.setState({
-        user: localStorage.getItem('user')
-      });
-      this.getMovies(accessToken);
-    }
-  }
-
-  onLoggedOut() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
+  /* NOT SURE THIS CODE IS CORRECT */
+  onRegistration(user) {
     this.setState({
-      user: null
+      user
     });
   }
 
-
-
-  /*When a movie is clicked, 
-  sthis function is invoked and 
-  updates the state of the `selectedMovie` *property to that movie*/
-
-  setSelectedMovie(movie) {
-    this.setState({
-      selectedMovie: movie
-    });
-  }
 
   render() {
-    console.log("render")
     const { movies, selectedMovie, user } = this.state;
-    // if (!user) return <RegistrationView onRegistration={user => this.onRegistration(user)} />;
+
+    /* If there is no user, the LoginView is rendered. 
+    If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    /*if (!user) return <RegistrationView onRegistration={user => this.onRegistration(user)} />; */
+
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
-
 
     /* Before the movies have been loaded */
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
+      <Container>
 
-      <Router>
+        <Navbar className="navbar"
+          collapseOnSelect
+          expand="lg"
+          variant="dark"
+          sticky="top">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src={Logo}
+                height="100"
+                className="d-inline-block align-top"
+              />{''}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="nav-link">
+                <Nav.Link href="#home" >Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Row className="main-view justify-content-md-center">
-          <Route exact path="/" render={() => {
-            console.log("rootpath");
-            return movies.map(movie => (
-              <Col md={3}>
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
+          {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned */}
+          {selectedMovie
+            ? (
+              // Do we need () around the <Row></Row>
+              <Col lg={8}>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
               </Col>
-            ))
-          }} />
-          <Route path="/movies/:movieId" render={({ match }) => {
-            console.log("movieview_path");
-            return <Col lg={8}>
-
-            </Col>
-          }} />
+            )
+            :
+            (
+              movies.map(movie => (
+                <Col md={3}>
+                  <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
+                </Col>
+              ))
+            )
+          }
         </Row>
-      </Router>
-
+      </Container >
     );
   }
 
